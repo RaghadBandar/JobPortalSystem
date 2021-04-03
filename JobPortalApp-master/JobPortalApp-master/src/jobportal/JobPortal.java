f58bd141104c901c1bfeec328ca88982ac173197
RaghadBandar <74097860+RaghadBandar@users.noreply.github.com>
2021-04-03 17:26:52 +0300

    first commit

---
+++ JobPortalApp-master/JobPortalApp-master/src/jobportal/JobPortal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobportal;
+
+//OOP2 Project Section#4/Group#4 
+
+//Raghad
+
+public class JobPortal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         screen mai = new screen();
+               mai.setVisible(true);
+         SignFrame e = new SignFrame();
+                 e.setVisible(true);
+    }
+    
+}
