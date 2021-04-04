b5778838335b3f98b53d807fbab25342e8865ebc
FatimalRamadan <2180004473@iau.edu.sa>
2021-04-04 20:35:57 +0300

    Update JobPortal.java

--- JobPortalApp-master/JobPortalApp-master/src/jobportal/JobPortal.java
+++ JobPortalApp-master/JobPortalApp-master/src/jobportal/JobPortal.java
@@ -7,14 +7,17 @@ package jobportal;
 
 //OOP2 Project Section#4/Group#4 
 
+//Fatima Al-Ramadhan 2180004473
 //Raghad bandar 2190005368
 //Razan Alsudairi 2180002990
+
 
 public class JobPortal {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
          screen mai = new screen();
