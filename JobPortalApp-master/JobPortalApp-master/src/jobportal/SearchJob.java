65054c70c10596a3d6da30883fcb1f3611461c0a
RaghadBandar <74097860+RaghadBandar@users.noreply.github.com>
2021-04-06 21:46:57 +0300

    JobTable Part2

--- JobPortalApp-master/JobPortalApp-master/src/jobportal/SearchJob.java
+++ JobPortalApp-master/JobPortalApp-master/src/jobportal/SearchJob.java
@@ -130,7 +130,9 @@ public class SearchJob extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField7ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        SelectJob x = new SelectJob();
+        x.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
